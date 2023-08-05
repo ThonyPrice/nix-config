96a4bd0470b5afe27c44fb4cef188ee3b82401f9
Thony Price <thony@Thonys-MacBook-Pro.local>
2023-08-05 02:32:01 +0200

    Pin personal unstable emacs fork

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -7,9 +7,9 @@ let
 
   pinnedSha256 =
     # Forked 2023-08-01
-    "a0928a82ae68f4697f39c6e0ffcba763ea02a66b";
+    "01q8bw40dh350zjx7g50ib4599sbsjgg711f794ib4d21ixh9xai";
   emacsUnstableFork =
-    "https://github.com/nix-community/emacs-overlay/archive/${pinnedSha256}.tar.gz";
+    "https://github.com/thonyprice/emacs-overlay/archive/${pinnedSha256}.tar.gz";
 
 in {
   emacs = (import (builtins.fetchTarball {
