1cf40156379fd8fee26b3cf7bf58a3cfbc7ca6d6
ThonyPrice <thonyprice@gmail.com>
2023-09-12 22:48:30 +0200

    Update Rofi config

--- modules/nixos/graphical/rofi.nix
+++ modules/nixos/graphical/rofi.nix
@@ -4,11 +4,37 @@
 
   config = lib.mkIf pkgs.stdenv.isLinux {
 
-    # Rofi
-    environment.systemPackages = with pkgs;
-      [
-        rofi-wayland
-      ];
+    home-manager.users.${config.user} = {
+
+      programs.rofi = {
+        enable = true;
+        cycle = true;
+        location = "center";
+        font = "Fira Code Font 11";
+        package = pkgs.rofi-wayland;
+        plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ];
+        extraConfig = {
+          show-icons = true;
+          kb-cancel = "Escape,Super+space";
+          modi = "window,run,ssh,emoji,calc";
+          sort = true;
+        };
+        theme = let
+          inherit (config.home-manager.users.${config.user}.lib.formats.rasi)
+            mkLiteral;
+        in {
+          "*" = {
+            background-color = mkLiteral "#1e1e2e";
+            foreground-color = mkLiteral "#cdd6f4";
+            border-color = mkLiteral "#1e1e2e";
+            selected-color = mkLiteral "#1e1e2e";
+            width = 360;
+          };
+        };
+
+      };
+
+    };
 
   };
 
