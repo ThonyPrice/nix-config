98778bd83ea845c234477a069defa6d751829d4a
Thony Price <thony@Thonys-MacBook-Pro.local>
2023-11-03 13:30:17 +0100

    Update flake

--- modules/common/shell/utilities.nix
+++ modules/common/shell/utilities.nix
@@ -48,15 +48,18 @@ in {
       programs.bat = {
         enable = true; # cat replacement
         themes = {
-          catppuccin-macchiato = builtins.readFile (pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "bat";
-            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-            sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-          } + "/Catppuccin-macchiato.tmTheme");
+          catppuccin = {
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat";
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            };
+            file = "Catppuccin-macchiato.tmTheme";
+          };
         };
         config = {
-          theme = "catppuccin-macchiato";
+          theme = "catppuccin";
           pager = "less -R"; # Don't auto-exit if one screen
         };
 
