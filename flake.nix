2b62e74481482703eb3ff197b44589b72c8e77de
Thony Price <thony@Thonys-MacBook-Pro.local>
2023-11-15 13:43:17 +0100

    Fix pluto hostname

--- flake.nix
+++ flake.nix
@@ -90,7 +90,8 @@
       # darwin-rebuild switch --flake .#pluto
       darwinConfigurations = {
         # MDM Managed, hence the funky hostname
-        pluto = import ./hosts/pluto { inherit inputs globals overlays; };
+        MAC-NL6CFNNHRP =
+          import ./hosts/pluto { inherit inputs globals overlays; };
       };
 
       # For quickly applying home-manager settings with:
