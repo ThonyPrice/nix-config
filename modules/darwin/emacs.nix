49f0a2c08d090f11f60285d34304b84e59ffa40e
ThonyPrice <thonyprice@gmail.com>
2024-09-02 22:04:52 +0200

    Consider emacs enable option for Darwin

--- modules/darwin/emacs.nix
+++ modules/darwin/emacs.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }: {
 
-  config = lib.mkIf pkgs.stdenv.isDarwin {
+  config = lib.mkIf (pkgs.stdenv.isDarwin && config.emacs.enable) {
 
     # Setup emacs path and daemon on login
     launchd.user.agents.emacs.path = [ config.environment.systemPath ];
