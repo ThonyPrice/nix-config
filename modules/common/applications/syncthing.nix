13ca055416145558333240afb4931cbbd039f538
thonyprice <thonyprice@gmail.com>
2025-01-09 08:42:25 +0100

    Configure syncthing

--- modules/common/applications/syncthing.nix
+++ modules/common/applications/syncthing.nix
@@ -12,11 +12,8 @@
         package = pkgs.syncthing;
         guiAddress = "127.0.0.1:8384";
 
-        # Don't override devices and folders added though
-        # GUI until I've figured out/ensured all devides
-        # are added correctly through Nix configs
-        overrideDevices = false;
         overrideFolders = false;
+        overrideDevices = false;
 
         settings = {
 
@@ -24,17 +21,20 @@
           options = { localAnnounceEnabled = false; };
 
           devices = {
-            galaxy-S21 = {
-              addresses = [ "tcp://192.168.0.10:51820" ];
+            galaxyS21 = {
               id =
-                "7CFNTQM-IMTJBHJ-3UWRDIU-ZGQJFR6-VCXZ3NB-XUH3KZO-N52ITXR-LAIYUAU";
+                "IT4OBHL-7AXGSL3-VBW7RTJ-K64NJTW-UA5VWTI-UEUCGWF-Z6XZJXP-RZM65Q4";
             };
           };
 
           folders = {
-            "~/org" = {
+            "~/git/org" = {
               id = "org";
               devices = [ "galaxyS21" ];
+            };
+            "~/Sync" = {
+              id = "sync";
+              devices = [ ];
             };
           };
 
